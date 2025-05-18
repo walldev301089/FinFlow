b698f2eea04973ac24b2505037fd90cabab614e0
walldev301089 <wallace89.dev@gmail.com>
2025-05-18 00:17:02 -0300

    foi feito modificações de segurança

--- backend/finflow/src/main/java/com/finflow/controller/UsuarioController.java
+++ backend/finflow/src/main/java/com/finflow/controller/UsuarioController.java
@@ -2,7 +2,7 @@ package com.finflow.controller;
 
 import com.finflow.entity.Usuario;
 import com.finflow.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +11,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
-public class UsuarioControler {
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+public class UsuarioController {
 
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public List<Usuario> listaDeUsuarios() {
         return usuarioRepository.findAll();
